0af76aa35567c00db92a453c0f83a5a549c36d3d
Zeroz04n <89253879+Pojan16@users.noreply.github.com>
2022-07-06 03:35:42 +0700

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -2,7 +2,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
     let user = global.db.data.users[m.sender]
     if (user.registered === true) return conn.sendButton(m.chat, `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`, wm, 'Unreg', `${usedPrefix}unreg`, m)
     let [name, age, gender] = text.split(/[&|.]/i)
-    let example = `\n\nContoh:\n${usedPrefix + command} Ariffb.19.cowo`
+    let example = `\n\nContoh:\n${usedPrefix + command} Pojan.16.cowo`
     if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)' + example
     if (!age) throw 'Umur tidak boleh kosong (Angka)' + example
     if (!gender) throw 'Jenis kelamin tidak boleh kosong (Alphanumeric)' + example
